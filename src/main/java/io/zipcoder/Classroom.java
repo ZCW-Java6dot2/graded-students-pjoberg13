4cffe126e97f212a30d1a0c632351ed8e4c91f0f
Peter <pjoberg13@gmail.com>
2020-11-09 22:09:35 -0500

    completed methods for graded students lab, need to complete and run tests

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,112 @@
 package io.zipcoder;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Classroom {
+    private Student[] students;
+
+    //nullary constructor
+    public Classroom() {
+        students = new Student[30];
+    }
+
+    public Classroom(Student[] students) {
+        this.students = students;
+    }
+
+    public Classroom(int maxNumberOfStudents) {
+        students = new Student[maxNumberOfStudents];
+    }
+
+    public Double getAverageExamScores() {
+        Double sumOfAverages = 0D;
+        for(int i = 0; i < students.length; i++) {
+            sumOfAverages += students[i].getAverageScore();
+        }
+        return sumOfAverages / students.length;
+    }
+
+    public void addStudent(Student student) {
+        for(int i = 0; i < students.length; i++) {
+            if(students[i] == null) {
+                students[i] = student;
+                break;
+            } else {
+                continue;
+            }
+        }
+    }
+
+    public void removeStudent(String firstName, String lastName) {
+        for(int i = 0; i < students.length; i++) {
+            if(students[i].getFirstName().equals(firstName)
+            && students[i].getLastName().equals(lastName)) {
+                students[i] = null;
+                break;
+            } else {
+                continue;
+            }
+        }
+        Arrays.sort(students);
+    }
+
+    public Student studentScoreLookUp(Double score) {
+        for(int i = 0; i < students.length; i++) {
+            if(students[i].equals(score)) {
+                return students[i];
+            } else {
+                continue;
+            }
+        }
+        return null;
+    }
+
+    public Student[] getStudentsByScore() {
+        Double[] scoresArray = new Double[students.length];
+        for(int i = 0; i < students.length; i++) {
+            scoresArray[i] = students[i].getAverageScore();
+        }
+        Arrays.sort(scoresArray);
+        Student[] studentsByScore = new Student[students.length];
+        for(int i = 0; i < scoresArray.length; i++) {
+            studentsByScore[i] = studentScoreLookUp(scoresArray[i]);
+        }
+        Arrays.sort(studentsByScore);
+        return studentsByScore;
+    }
+
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public Map<String, Character> getGradeBook() {
+        Map<String, Character> gradeBook = new HashMap<>();
+
+        int count, percent;
+
+        for(int i = 0; i < students.length; i++) {
+            count = 0;
+            for(int j = 0; j < students.length; j++) {
+                if(students[i].getAverageScore() > students[j].getAverageScore()) {
+                    count++;
+                }
+            }
+            percent = (count * 100) / (students.length - 1);
+            if(percent > 89) {
+                gradeBook.put(students[i].getLastName(), 'A');
+            } else if(percent <= 89 && percent > 70) {
+                gradeBook.put(students[i].getLastName(), 'B');
+            } else if(percent <= 70 && percent > 49) {
+                gradeBook.put(students[i].getLastName(), 'C');
+            } else if(percent <= 49 && percent > 11) {
+                gradeBook.put(students[i].getLastName(), 'D');
+            } else {
+                gradeBook.put(students[i].getLastName(), 'F');
+            }
+        }
+
+        return gradeBook;
+    }
 }
